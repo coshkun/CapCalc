c1ef32dbb5df398b599f09a3021d4e1f89f4f68b
coshkun <cskncnr@gmail.com>
2016-02-12 07:50:58 +0200

    FirstUploadToGitHub
    Push & Publish for first time

---
+++ README.md
@@ -0,0 +1,7 @@
+# 3dCamSysTest
+
+Release test for linux based systems for one of my old projects.
+
+####Notes:
+* if you came here accidentally, pls keep going to your surf to find your next killer app.
+* This is a nuke zone for testing my projects. Have a nice day! :+1:
---
+++ cleanersc.bat
@@ -0,0 +1,18 @@
+@echo off
+echo %cd..%
+echo %cd..%
+echo %cd C:\Users\owner\Documents\GitHub\CapCalc%
+echo "this will delete some files within the directory above!  Make sure unity is not running!"
+pause
+echo "are you sure you would like to do this?"
+pause
+rd /s /q Library
+rd /s /q Temp
+echo "skipping: del /s /q /f *.csproj"
+del /s /q /f *.pidb
+echo "skipping: del /s /q /f *.unityproj"
+del /s /q /f *.DS_Store
+echo "skipping: del /s /q /f *.sln"
+del /s /q /f *.userprefs
+echo "done."
+pause
