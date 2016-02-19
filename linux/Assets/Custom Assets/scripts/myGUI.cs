24d12f841d81b45f9bd7b8fd98fa1d4112fc3f2a
Coskun Caner <cskncnr@gmail.com>
2016-02-19 21:27:41 +0200

    commit new linux files

---
+++ linux/Assets/Custom Assets/scripts/myGUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myGUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		// Make a background for the interface
+		GUI.Box (new Rect (10, 10, 100, 300), "Menu");
+	}
+}
